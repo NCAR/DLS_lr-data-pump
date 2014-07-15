5425d6a516bbb50743dba0ade3b2e69c91603332
finkedave <dfinke@DLS-Huron.dls.ucar.edu>
2014-07-15 16:55:10 -0600

    adding copyright paragraph to all core files and added a general license file

--- datapump/run.py
+++ datapump/run.py
@@ -1,5 +1,19 @@
+#Copyright 2011 SRI International
+#Modifications Copyright 2014 University Corporation for Atmospheric Research (UCAR)
+#
+#Licensed under the Apache License, Version 2.0 (the "License");
+#you may not use this file except in compliance with the License.
+#You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+#Unless required by applicable law or agreed to in writing, software
+#distributed under the License is distributed on an "AS IS" BASIS,
+#WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#See the License for the specific language governing permissions and
+#limitations under the License.
+
 from __future__ import division
-import sys
 from datetime import datetime
 import logging
 import oai_node
@@ -11,11 +25,6 @@ from datapump.exception import PublishException
 import importlib
 import os
 from optparse import OptionParser
-'''
-Created on Feb 15, 2013
-
-@author: dfinke
-'''
 
 log = logging.getLogger(__name__)
 
